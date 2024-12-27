5248c5601ea7478f1d95c26c94c110270f91540a
Federico Salinas Samaniego <salinasfederico773@gmail.com>
2024-12-27 12:39:11 -0600

    Add TEC data extraction file

---
+++ PTEX_Extract_DTEC.py
@@ -0,0 +1,18 @@
+from scripts.VTECDataReader import VTECDataReader
+
+if __name__ == "__main__":
+    PTEX_dir = ["/home/federico/Documents/FCFM/Proyecto TIDs/Data/CMN Files/PTEX/2018",
+            "/home/federico/Documents/FCFM/Proyecto TIDs/Data/CMN Files/PTEX/2019"]
+    
+    PTEX_VTEC = VTECDataReader(dirs = PTEX_dir, min_amplitude = 0.1632)
+
+    PTEX_VTEC.read_and_extract_vtec_data()
+
+    with open("./data/PTEX_DTEC_series.dat", "+w") as PTEXout:
+        N = len(PTEX_VTEC.dtec_sequences)
+
+        PTEXout.write(f"{N}\n")
+
+        for n in range(N):
+            PTEXout.write(", ".join(list(map(lambda x: str(x), PTEX_VTEC.time_sequences[n]))) + "\n")
+            PTEXout.write(", ".join(list(map(lambda x: str(x), PTEX_VTEC.dtec_sequences[n]))) + "\n")
