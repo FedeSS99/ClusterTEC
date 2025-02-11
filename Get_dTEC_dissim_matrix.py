5d885c2e6775e7c3ec2b5ac470fbcc809764793b
Federico Salinas <salinasfederico773@gmail.com>
2025-02-10 22:53:57 -0600

    Add subseries file saving

--- Get_dTEC_dissim_matrix.py
+++ Get_dTEC_dissim_matrix.py
@@ -20,25 +20,29 @@ if __name__ == "__main__":
         exit()
 
     total_series = int(file_lines[0])
-
     with tqdm_joblib(tqdm(total= total_series)) as progress_bar:
-        dtec_series = Parallel(n_jobs=-1)(delayed(extract_prominent_series)(n,
+        dtec_subseries = Parallel(n_jobs=-1)(delayed(extract_prominent_series)(n,
                                     array([datetime.fromisoformat(x.strip()) for x in file_lines[n-1].split(",")]),
                                     array(file_lines[n].split(","), dtype=float),
                                     wavelet, dj) for n in range(2, 2*total_series + 1, 2))
 
-    dtec_series = [s for s in sorted(dtec_series, key=lambda x: x[0]) if len(s) == 3]
-    dtec_series = tuple(s[2] for s in dtec_series)
-    total_series = len(dtec_series)
+    dtec_subseries = [s for s in sorted(dtec_subseries, key=lambda x: x[0]) if len(s) == 3]
+    time_subseries = tuple(s[1] for s in dtec_subseries)
+    dtec_subseries = tuple(s[2] for s in dtec_subseries)
+    total_series = len(dtec_subseries)
     print(f"Total of series: {total_series}")
+    print("-- Saving dTEC subseries --")
+    with open("./data/PTEX_DTEC_subseries.dat", "+w") as PTEX_sub_out:
+        PTEX_sub_out.write(f"{total_series}\n")
+        for n in tqdm(range(total_series)):
+            PTEX_sub_out.write(", ".join(list(map(lambda x: str(x), time_subseries[n]))) + "\n")
+            PTEX_sub_out.write(", ".join(list(map(lambda x: str(x), dtec_subseries[n]))) + "\n")
 
     print(f"\n--Computing DTW between every pair of prominent series--")
-
     def compute_dist(index_1, index_2, seq_1, seq_2):
         return index_1, index_2, dtw(seq_1, seq_2)
-
     with tqdm_joblib(tqdm(total=total_series * (total_series - 1) // 2)) as progress_bar:
-        dtw_results = Parallel(n_jobs=-1)(delayed(compute_dist)(i, j, dtec_series[i], dtec_series[j])
+        dtw_results = Parallel(n_jobs=-1)(delayed(compute_dist)(i, j, dtec_subseries[i], dtec_subseries[j])
                                           for i in range(total_series - 1) for j in range(i + 1, total_series))
 
     dissimilarity_matrix = zeros((total_series, total_series))
