227b082c29ace84a62e66d2644800906afe86e6f
Federico Salinas <salinasfederico773@gmail.com>
2025-07-11 23:41:50 -0600

    Changed data input from file to VTEC reader

--- Get_dTEC_dissim_matrix.py
+++ Get_dTEC_dissim_matrix.py
@@ -1,33 +1,32 @@
+from scripts.VTECDataReader import VTECDataReader
 from scripts.subscripts.MiscellanousFunctions import tqdm_joblib
 from scripts.WaveletExtraction import extract_prominent_series
 
 from tqdm import tqdm
 from joblib import Parallel, delayed
 
-from datetime import datetime
 from tslearn.metrics import dtw
-from numpy import array, zeros, savetxt, datetime64
+from numpy import zeros, savetxt
 
 if __name__ == "__main__":
+    PTEX_dir = ["/home/federico/Documents/FCFM/Proyecto TIDs/Data/CMN Files/PTEX/2018",
+        "/home/federico/Documents/FCFM/Proyecto TIDs/Data/CMN Files/PTEX/2019"]
     wavelet = "cmor1.5-1.5"
     dj = 0.0625
 
-    try:
-        with open("./data/PTEX_dtec_series.dat", "r") as DTECin:
-            file_lines = DTECin.readlines()
-    except FileNotFoundError:
-        print("Error: El archivo no fue encontrado.")
-        exit()
+    PTEX_VTEC = VTECDataReader(dirs = PTEX_dir, min_amplitude = 0.17334, window_size = 240)
+    PTEX_VTEC.read_and_extract_vtec_data()
+    total_series = len(PTEX_VTEC.dtec_sequences)
 
-    total_series = int(file_lines[0])
-    prn_subseries = tuple([int(file_lines[n-2]) for n in range(3, 3*total_series + 1, 3)])
+    print("--Extracting prominent dTEC subseries--")
     with tqdm_joblib(tqdm(total= total_series)) as progress_bar:
         dtec_subseries = Parallel(n_jobs=-1)(delayed(extract_prominent_series)(n,
-                                    array([datetime.fromisoformat(x.strip()) for x in file_lines[n-1].split(",")], dtype = datetime64),
-                                    array(file_lines[n].split(","), dtype=float),
-                                    wavelet, dj) for n in range(3, 3*total_series + 1, 3))
+                                    PTEX_VTEC.time_sequences[n],
+                                    PTEX_VTEC.dtec_sequences[n],
+                                    wavelet, dj) for n in range(total_series))
 
     dtec_subseries = [s for s in sorted(dtec_subseries, key=lambda x: x[0]) if len(s) == 3]
+    prn_subseries = tuple(PTEX_VTEC.prn_sequences[s[0]] for s in dtec_subseries)
     time_subseries = tuple(s[1] for s in dtec_subseries)
     dtec_subseries = tuple(s[2] for s in dtec_subseries)
     total_series = len(dtec_subseries)
