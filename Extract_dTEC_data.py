c6fdaaf8adc6003dc47e5c42bd5edeae3148a3c1
Federico Salinas <salinasfederico773@gmail.com>
2025-07-11 23:41:23 -0600

    Modified dTEC threshold

--- Extract_dTEC_data.py
+++ Extract_dTEC_data.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     PTEX_dir = ["/home/federico/Documents/FCFM/Proyecto TIDs/Data/CMN Files/PTEX/2018",
             "/home/federico/Documents/FCFM/Proyecto TIDs/Data/CMN Files/PTEX/2019"]
     
-    PTEX_VTEC = VTECDataReader(dirs = PTEX_dir, min_amplitude = 0.18695, window_size = 240)
+    PTEX_VTEC = VTECDataReader(dirs = PTEX_dir, min_amplitude = 0.17334, window_size = 240)
 
     PTEX_VTEC.read_and_extract_vtec_data()
 
